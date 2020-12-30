2adb57fc8a027c1c34e7b57bcdd974a9c38e163a
Petya Zhekova <71344477+petruna93@users.noreply.github.com>
2020-12-30 20:14:54 +0200

    Data Types and Variables - Lab

---
+++ DataTypesAndVariablesLab/CharsToString.java
@@ -0,0 +1,14 @@
+package com.company.DataTypesAndVariablesLab;
+
+import java.util.Scanner;
+
+public class CharsToString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char first = scanner.nextLine().charAt(0);
+        char second = scanner.nextLine().charAt(0);
+        char third = scanner.nextLine().charAt(0);
+
+        System.out.printf("%c%c%c", first,second,third);
+    }
+}
---
+++ DataTypesAndVariablesLab/ConvertMetersToKilometers.java
@@ -0,0 +1,15 @@
+package com.company.DataTypesAndVariablesLab;
+
+import java.util.Scanner;
+
+public class ConvertMetersToKilometers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int meters = Integer.parseInt(scanner.nextLine());
+        double kilometers = meters / 1000.0;
+
+        System.out.println(String.format( "%.2f",kilometers));
+    }
+}
