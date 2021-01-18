5ad5cea6b1755aba7cbfdae5593bf47e71e4fbe6
Petya Zhekova <71344477+petruna93@users.noreply.github.com>
2021-01-18 22:40:19 +0200

    Regular Expressions - Lab

---
+++ RegularExpressionsLab/MatchDates.java
@@ -0,0 +1,23 @@
+package com.company.RegularExpressionsLab;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MatchDates {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String regex = "\\b(?<day>\\d{2})([-.\\/])(?<month>[A-Z][a-z]{2})\\2(?<year>\\d{4})\\b";
+        String text = scanner.nextLine();
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+
+
+        while (matcher.find()) {
+            System.out.println(String.format("Day: %s, Month: %s, Year: %s", matcher.group("day"),
+                    matcher.group("month"), matcher.group("year")));
+
+        }
+    }
+}
